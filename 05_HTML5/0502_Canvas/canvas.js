673b430cc085ccf44bd8335b636fa7a2b8de3e2a
Jason Lee <leemin0830@gmail.com>
2014-05-03 23:58:26 +0900

    canvas example updated

--- 05_HTML5/0502_Canvas/canvas.js
+++ 05_HTML5/0502_Canvas/canvas.js
@@ -1,64 +1,63 @@
 var canvas;
 var ctx;
 var toolType;
-var mousePressed=false;
+var mousePressed = false;
 
 function init() {
-	canvas=document.getElementById('mainCanvas');
-	canvas.addEventListener('mousedown',onmousedown);
-	canvas.addEventListener('mouseup',onmouseup);
-	document.addEventListener('mouseup',onmouseup);
-	canvas.addEventListener('mousemove',onmousemove);
-
-	ctx=canvas.getContext('2d');
+	canvas = document.getElementById('mainCanvas');
+	canvas.addEventListener('mousedown', onmousedown);
+	canvas.addEventListener('mouseup', onmouseup);
+	document.addEventListener('mouseup', onmouseup);
+	canvas.addEventListener('mousemove', onmousemove);
+	ctx = canvas.getContext('2d');
 }
 
-function onmousedown(e){
-	mousePressed=true;
-	toolType=document.getElementById('toolType').value;
+function onmousedown(e) {
+	mousePressed = true;
+	toolType = document.getElementById('toolType').value;
 	ctx.beginPath();
-	ctx.strokeStyle=document.getElementById('color').value;
-	ctx.moveTo(e.x-canvas.offsetLeft,e.y-canvas.offsetTop);
-	draw(e.x-canvas.offsetLeft, e.y-canvas.offsetTop);
+	ctx.strokeStyle = document.getElementById('color').value;
+	ctx.moveTo(e.x - canvas.offsetLeft, e.y - canvas.offsetTop);
+	draw(e.x - canvas.offsetLeft, e.y - canvas.offsetTop);
 }
 
-function onmouseup(e){
-	mousePressed=false;
+function onmouseup(e) {
+	mousePressed = false;
 }
 
-function onmousemove(e){
-	if(mousePressed)
-		draw(e.x-canvas.offsetLeft,e.y-canvas.offsetTop);
+function onmousemove(e) {
+	if (mousePressed)
+		draw(e.x - canvas.offsetLeft, e.y - canvas.offsetTop);
 }
 
-function draw(x,y){
+function draw(x, y) {
 	switch (toolType) {
 	case 'line':
-		drawLine(x,y);
+		drawLine(x, y);
 		break;
 	case 'rect':
-		drawRect(x,y);
+		drawRect(x, y);
 		break;
 	case 'circle':
-		drawCircle(x,y);
+		drawCircle(x, y);
 		break;
-	}	
+	}
 }
 
-function drawLine(x,y){
-	ctx.lineTo(x,y);
+function drawLine(x, y) {
+	ctx.lineTo(x, y);
 	ctx.stroke();
 }
 
-function drawRect(x,y){
-	ctx.rect(x,y,50,50);
-	ctx.stroke();	
+function drawRect(x, y) {
+	ctx.rect(x, y, 50, 50);
+	ctx.stroke();
 }
 
-function drawCircle(x,y){
+function drawCircle(x, y) {
 	ctx.beginPath();
-	ctx.arc(x,y,50,0,2*Math.PI);
-	ctx.stroke();	
+	ctx.arc(x, y, 50, 0, 2 * Math.PI);
+	ctx.stroke();
 }
 
 onload = init;
